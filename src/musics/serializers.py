646dbcb3aaac352d011a0886ef8f1d3c8169bc33
Joonki <jkkim@nextree.co.kr>
2017-06-23 23:34:54 +0900

    fix bug - serializers.py(musics)

--- src/musics/serializers.py
+++ src/musics/serializers.py
@@ -9,7 +9,7 @@ from rest_framework import serializers
 class MusicSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Music
-        fields = ('id','numberOfGradings','sumOfGrades','composer','name', 'koreanName', 'genre', 'instrument','compositionYear')
+        fields = ('id','composer','name', 'koreanName', 'genre', 'instrument','compositionYear')
         
 class MusicReviewSerializer(serializers.ModelSerializer):
     class Meta:
